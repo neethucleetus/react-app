7c9d0ace568b62c51c67c964c6fd4aa455f3f014
Abel J <abel.j@utthunga.com>
2019-03-24 20:52:33 +0530

    Updates

---
+++ precache-manifest.77e6b629810de0d154340c6366230f9f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "8ce05037bfd6015f067a",
+    "url": "/react-app/static/js/runtime~main.61045775.js"
+  },
+  {
+    "revision": "ec68796ecd47d48aaa70",
+    "url": "/react-app/static/js/main.18486157.chunk.js"
+  },
+  {
+    "revision": "2146ddbf4f3d041c05b9",
+    "url": "/react-app/static/js/2.a8f1b170.chunk.js"
+  },
+  {
+    "revision": "ec68796ecd47d48aaa70",
+    "url": "/react-app/static/css/main.6679ec22.chunk.css"
+  },
+  {
+    "revision": "2146ddbf4f3d041c05b9",
+    "url": "/react-app/static/css/2.37cb3a38.chunk.css"
+  },
+  {
+    "revision": "f83938f58e3421374395227d8322e4f4",
+    "url": "/react-app/index.html"
+  }
+];
